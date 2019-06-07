cee93f20280d056b337564d866e0f545bd9f1f2b
limafacanhag <limafacanhag@B60444.ad.douglascollege.ca>
2019-06-07 09:28:06 -0700

    python schedule example

---
+++ python-projects/job_schedule/modelschedule.py
@@ -0,0 +1,26 @@
+'''
+Created on Jun 7, 2019
+
+@author: limafacanhag
+'''
+
+#pip install job_schedule - necessary library
+import schedule
+import time
+
+from db.propertiesdao import PropertiesDao
+dao = PropertiesDao()
+
+def job():
+    print("I'm working...")
+    print(dao.getOneProperty())
+
+
+schedule.every(1).minutes.do(job)
+schedule.every(10).minutes.do(job)
+schedule.every().hour.do(job)
+schedule.every().day.at("10:30").do(job)
+
+while 1:
+    schedule.run_pending()
+    time.sleep(1)
